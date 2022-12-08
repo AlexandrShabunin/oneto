c21c4c25f324719f2d15e6dc545aad70967fe057
AlexandrShah <hugoboss11@yandex.ru>
2022-12-08 21:51:00 +0500

    нахождение большего чесла из двух чисел

---
+++ one/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("введите number1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите number2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("number1 больше number2");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine("number2 больше number1");
+}
