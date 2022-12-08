9c206454b9d70c0086106ea26102215604cce57f
AlexandrShah <hugoboss11@yandex.ru>
2022-12-08 23:14:03 +0500

    нахождение максимального числа из трех чисел

---
+++ too/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+console.Write("ввести первое число:");
+int number1 = int.Parse(Console.ReadLine());
+console.Write("ввести второе число:");
+int number2 = int.Parse(console.ReadLine());
+console.Write("ввести nhtnmt число:");
+int number3 = int.Parse(console.ReadLine());
+
+int max = numder1;
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+Console.WriteLine("максимальное число=max");
+
