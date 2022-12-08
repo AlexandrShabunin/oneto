30edd7b4935bb60769c540f3a0aeb0b62222e43e
AlexandrShah <hugoboss11@yandex.ru>
2022-12-08 23:48:29 +0500

    нахождение числа . все четные числа до N

---
+++ 4/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+console.WriteLine("введите N: ");
+int N = int.Parse(console.ReadLine());
+int a = 1;
+while (a <= N)
+{
+    if (false % 2 ==0)
+    {
+        console.Write("a");
+    }
+}
